0484d9606b5c6a62ffd9229073d30c0aa4f81797
Nazeh <ar.nazeh@gmail.com>
2019-07-09 13:08:17 +0300

    update test casses with no style errors

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -12,47 +12,47 @@ my_array = (1..4).to_a
 
 # EACH
 my_array.each { |e| p e }
-my_array.my_each { |e| p e }
+my_array.my_each { |e| p e * 2 }
 
 # EACH_WITH_INDEX
-my_array.each_with_index { |e, i| p "element: #{e}, index: #{i}" }
-my_array.my_each_with_index { |e, i| p "element: #{e}, index: #{i}" }
+my_array.each_with_index { |_e, _i| p 'element: # {e}, index: # {i}' }
+my_array.my_each_with_index { |_e, _i| p 'element: # {e}, index: # {i}' }
 
 # SELECT
-p my_array.select(&:even?)
-p my_array.my_select(&:even?)
+p(my_array.select(&:even?))
+p(my_array.my_select(&:even?))
 
 # ALL?
-p my_array.all? { |e| e > 1 }
-p my_array.my_all? { |e| e > 1 }
+p(my_array.all? { |e| e > 1 })
+p(my_array.my_all? { |e| e > 1 })
 
 # ANY?
-p my_array.any? { |e| e < 1 }
-p my_array.my_any? { |e| e < 1 }
+p(my_array.any? { |e| e < 1 })
+p(my_array.my_any? { |e| e < 1 })
 
 # NONE?
-p my_array.none? { |e| e > 1 }
-p my_array.my_none? { |e| e > 1 }
+p(my_array.none? { |e| e > 1 })
+p(my_array.my_none? { |e| e > 1 })
 
 # COUNT
-p my_array.count(&:odd?)
-p my_array.my_count(&:odd?)
-p my_array.count 3
-p my_array.my_count 3
+p(my_array.count(&:odd?))
+p(my_array.my_count(&:odd?))
+p(my_array.count(3))
+p(my_array.my_count(3))
 
 # MAP
-p my_array.map { |e| e**2 }
-p my_array.my_map { |e| e**2 }
+p(my_array.map { |e| e**2 })
+p(my_array.my_map { |e| e**2 })
 proc = proc { |e| e**2 }
-p my_array.my_map(proc)
+p(my_array.my_map(proc))
 
 # INJECT
-p my_array.inject(:+)
-p my_array.my_inject(:+)
-p my_array.inject(1) { |product, n| product * n }
-p my_array.my_inject(1) { |product, n| product * n }
-p my_array.inject(1, :*)
-p my_array.my_inject(1, :*)
-p my_array.inject { |sum, n| sum + n }
-p my_array.my_inject { |sum, n| sum + n }
+p(my_array.inject(:+))
+p(my_array.my_inject(:+))
+p(my_array.inject(1) { |product, n| product * n })
+p(my_array.my_inject(1) { |product, n| product * n })
+p(my_array.inject(1, :*))
+p(my_array.my_inject(1, :*))
+p(my_array.inject { |sum, n| sum + n })
+p(my_array.my_inject { |sum, n| sum + n })
 p multiply_els([2, 4, 5])
